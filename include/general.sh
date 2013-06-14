cca3842921db3b576cbd2d8ccbed4c82312d6c10
Tony He <cvvnx1@163.com>
2013-06-14 22:57:32 +0800

    test supportpkglist.conf v2

--- include/general.sh
+++ include/general.sh
@@ -51,7 +51,7 @@ general_checktoolexist_null(){
 general_checkpkgsupport_null(){
   default_installopt=$(cat ${script_path}/src/supportpkglist.conf | grep "$1" | sed "s/.*|//")
   if [ -z "${default_installopt}" ]; then
-    echo "$1 is not suuported by adss"
+    echo "$1 is not supported by adss"
     exit 1
   fi
 }
